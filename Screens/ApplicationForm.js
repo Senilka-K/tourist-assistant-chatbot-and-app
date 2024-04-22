1f9e4284707d5a9b4736f127f3b402a899dbcf54
Senilka <senilka0108@gmail.com>
2024-04-22 15:00:38 +0530

    wip

--- Screens/ApplicationForm.js
+++ Screens/ApplicationForm.js
@@ -7,6 +7,7 @@ import {
   KeyboardAvoidingView,
   TouchableOpacity,
   Platform,
+  ScrollView,
   Alert,
 } from "react-native";
 import { useState } from "react";
@@ -26,6 +27,15 @@ export default function ApplicationForm() {
       Alert.alert("Error", "Please fill in all fields.");
     } else {
       Alert.alert("Success", "Form Submitted Successfully!");
+      // Clear the form fields
+      setName("");
+      setArriveDate("");
+      setDistrict("");
+      setContactNo("");
+      setPassportId("");
+      setComment("");
+      setLikesTracking(null);
+      setErrors({});
     }
   };
 
@@ -46,112 +56,129 @@ export default function ApplicationForm() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Application Form</Text>
-      <View style={styles.form}>
-        <Text style={styles.label}>Name:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your Name"
-          value={name}
-          onChangeText={setName}
-        />
-        {errors.name ? (
-          <Text style={styles.errorText}>{errors.name}</Text>
-        ) : null}
-        <Text style={styles.label}>Arrive Date:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your Arrival Date"
-          value={arriveDate}
-          onChangeText={setArriveDate}
-        />
-        {errors.arriveDate ? (
-          <Text style={styles.errorText}>{errors.arriveDate}</Text>
-        ) : null}
-        <Text style={styles.label}>Hope to go district:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your hope to go district"
-          value={district}
-          onChangeText={setDistrict}
-        />
-        {errors.district ? (
-          <Text style={styles.errorText}>{errors.district}</Text>
-        ) : null}
-        <Text style={styles.label}>Contact No:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your Contact No"
-          value={contactNo}
-          onChangeText={setContactNo}
-        />
-        {errors.contactNo ? (
-          <Text style={styles.errorText}>{errors.contactNo}</Text>
-        ) : null}
-        <Text style={styles.label}>Passport Id:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your Passport Id"
-          value={passportId}
-          onChangeText={setPassportId}
-        />
-        {errors.passportId ? (
-          <Text style={styles.errorText}>{errors.passportId}</Text>
-        ) : null}
-        <Text style={styles.label}>Comment:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your Comment"
-          value={comment}
-          onChangeText={setComment}
-        />
-        {errors.comment ? (
-          <Text style={styles.errorText}>{errors.comment}</Text>
-        ) : null}
-        <Text style={styles.label}>Do you like Tracking</Text>
-        <View style={styles.buttonGroup}>
-          <TouchableOpacity
-            style={[
-              styles.button,
-              likesTracking === "yes" ? styles.buttonSelected : null,
-            ]}
-            onPress={() => setLikesTracking("yes")}
-          >
-            <Text style={styles.buttonText}>Yes</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[
-              styles.button,
-              likesTracking === "no" ? styles.buttonSelected : null,
-            ]}
-            onPress={() => setLikesTracking("no")}
-          >
-            <Text style={styles.buttonText}>No</Text>
-          </TouchableOpacity>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+    >
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Application Form</Text>
+          <View style={styles.form}>
+            <Text style={styles.label}>Name:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Name"
+              value={name}
+              onChangeText={setName}
+            />
+            {errors.name ? (
+              <Text style={styles.errorText}>{errors.name}</Text>
+            ) : null}
+            <Text style={styles.label}>Arrive Date:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Arrival Date"
+              value={arriveDate}
+              onChangeText={setArriveDate}
+            />
+            {errors.arriveDate ? (
+              <Text style={styles.errorText}>{errors.arriveDate}</Text>
+            ) : null}
+            <Text style={styles.label}>Hope to go district:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your hope to go district"
+              value={district}
+              onChangeText={setDistrict}
+            />
+            {errors.district ? (
+              <Text style={styles.errorText}>{errors.district}</Text>
+            ) : null}
+            <Text style={styles.label}>Contact No:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Contact No"
+              value={contactNo}
+              onChangeText={setContactNo}
+            />
+            {errors.contactNo ? (
+              <Text style={styles.errorText}>{errors.contactNo}</Text>
+            ) : null}
+            <Text style={styles.label}>Passport Id:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Passport Id"
+              value={passportId}
+              onChangeText={setPassportId}
+            />
+            {errors.passportId ? (
+              <Text style={styles.errorText}>{errors.passportId}</Text>
+            ) : null}
+            <Text style={styles.label}>Comment:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Comment"
+              value={comment}
+              onChangeText={setComment}
+            />
+            {errors.comment ? (
+              <Text style={styles.errorText}>{errors.comment}</Text>
+            ) : null}
+            <Text style={styles.label}>Do you like Tracking</Text>
+            <View style={styles.buttonGroup}>
+              <TouchableOpacity
+                style={[
+                  styles.button,
+                  likesTracking === "yes" ? styles.buttonSelected : null,
+                ]}
+                onPress={() => setLikesTracking("yes")}
+              >
+                <Text style={styles.buttonText}>Yes</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  styles.button,
+                  likesTracking === "no" ? styles.buttonSelected : null,
+                ]}
+                onPress={() => setLikesTracking("no")}
+              >
+                <Text style={styles.buttonText}>No</Text>
+              </TouchableOpacity>
+            </View>
+            {errors.likesTracking ? (
+              <Text style={styles.errorText}>{errors.likesTracking}</Text>
+            ) : null}
+            <View style={styles.actionButtonGroup}>
+              <TouchableOpacity
+                style={styles.actionButton}
+                onPress={handleDone}
+              >
+                <Text style={styles.actionButtonText}>Done</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.actionButton}
+                onPress={handleEdit}
+              >
+                <Text style={styles.actionButtonText}>Edit</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.actionButton}
+                onPress={handleDelete}
+              >
+                <Text style={styles.actionButtonText}>Delete</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-        {errors.likesTracking ? (
-          <Text style={styles.errorText}>{errors.likesTracking}</Text>
-        ) : null}
-        <View style={styles.actionButtonGroup}>
-          <TouchableOpacity style={styles.actionButton} onPress={handleDone}>
-            <Text style={styles.actionButtonText}>Done</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.actionButton} onPress={handleEdit}>
-            <Text style={styles.actionButtonText}>Edit</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.actionButton} onPress={handleDelete}>
-            <Text style={styles.actionButtonText}>Delete</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: "center",
     alignItems: "center",
   },

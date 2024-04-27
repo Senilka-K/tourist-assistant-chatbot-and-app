67201070a32a4349834d96d96e0d49d3ffae1563
Senilka <senilka0108@gmail.com>
2024-04-27 19:08:01 +0530

    form-done

--- Backend/server.js
+++ Backend/server.js
@@ -164,6 +164,22 @@ mongoose.connect('mongodb+srv://senilka0108:abcd4321@cluster0.r55v5qx.mongodb.ne
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
+// Add User endpont
+  app.post('/add-user', async (req, res) => {
+    try {
+      // Create a new user using the data in the request body
+      const newUser = new User(req.body);
+  
+      // Save the user to the database
+      await newUser.save();
+  
+      // Send back the created user
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ message: 'Error creating the user', error: error });
+    }
+  });
+
 // User login endpoint
 app.post('/login', async (req, res) => {
   const { username } = req.body;
@@ -180,7 +196,8 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.get('/formData', async (req, res) => {
+// Already filled formData getting endpoint
+app.post('/formData', async (req, res) => {
   const { userId } = req.body; 
   try {
     const formData = await FormDetails.findOne({ userId: userId });
@@ -193,14 +210,59 @@ app.get('/formData', async (req, res) => {
   }
 });
 
+// Form submition endpoint
 app.post('/submit-form', async (req, res) => {
   const formData = new FormDetails(req.body);
   try {
     await formData.save();
-    res.status(200).json({ message: 'Form data received successfully.' });
+    res.status(200).json({ message: 'Form data saved successfully.' });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Error saving form data', error: error.message });
+  }
+});
+
+// Form delete endpoint
+app.delete('/delete-form', async (req, res) => {
+  const { userId } = req.body;
+  if (!userId) {
+    return res.status(400).json({ message: 'User ID is required' });
+  }
+  try {
+    const result = await FormDetails.findOneAndDelete({ userId: userId });
+    if (!result) {
+      return res.status(404).json({ message: 'Form data not found for the given user' });
+    }
+    res.status(200).json({ message: 'Form data deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error deleting form data', error: error.message });
+  }
+});
+ 
+//Form edit endpoint
+app.put('/edit-form', async (req, res) => {
+  const { userId } = req.body; 
+  console.log(userId);
+  try {
+    const formData = await FormDetails.findOne({ userId: userId });
+    if (!formData) {
+      return res.status(404).json({ message: 'Form data not found for the given user ID' });
+    }
+    formData.name = req.body.name || formData.name;
+    formData.arriveDate = req.body.arriveDate || formData.arriveDate;
+    formData.district = req.body.district || formData.district;
+    formData.contactNo = req.body.contactNo || formData.contactNo;
+    formData.passportId = req.body.passportId || formData.passportId;
+    formData.emergencyNo = req.body.emergencyNo || formData.emergencyNo;
+    formData.comment = req.body.comment || formData.comment;
+    formData.likesTracking = req.body.likesTracking !== undefined ? req.body.likesTracking : formData.likesTracking;
+    
+    await formData.save();
+    res.status(200).json({ message: 'Form updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error updating form data', error: error.message });
   }
 });
 
--- MobileApp/Screens/ApplicationForm.js
+++ MobileApp/Screens/ApplicationForm.js
@@ -1,278 +1,3 @@
-// import {
-//   Dimensions,
-//   StyleSheet,
-//   Text,
-//   View,
-//   Button,
-//   TextInput,
-//   KeyboardAvoidingView,
-//   TouchableOpacity,
-//   Platform,
-//   ScrollView,
-//   Alert,
-// } from "react-native";
-// import { useState } from "react";
-
-// const screenWidth = Dimensions.get("window").width;
-
-// export default function ApplicationForm() {
-//   const [name, setName] = useState("");
-//   const [arriveDate, setArriveDate] = useState("");
-//   const [district, setDistrict] = useState("");
-//   const [contactNo, setContactNo] = useState("");
-//   const [passportId, setPassportId] = useState("");
-//   const [emergencyNo, setEmergencyNo] = useState("");
-//   const [comment, setComment] = useState("");
-//   const [likesTracking, setLikesTracking] = useState(null);
-//   const [errors, setErrors] = useState({});
-
-//   const handleDone = () => {
-//     if (!name || !arriveDate || !district || !contactNo || !passportId) {
-//       Alert.alert("Error", "Please fill in all fields.");
-//     } else {
-//       Alert.alert("Success", "Form Submitted Successfully!");
-//       // Clear the form fields
-//       setName("");
-//       setArriveDate("");
-//       setDistrict("");
-//       setContactNo("");
-//       setEmergencyNo("");
-//       setPassportId("");
-//       setComment("");
-//       setLikesTracking(null);
-//       setErrors({});
-//     }
-//   };
-
-//   const handleEdit = () => {
-//     Alert.alert("Edit", "You can now edit the form.");
-//   };
-
-//   const handleDelete = () => {
-//     Alert.alert(
-//       "Delete",
-//       "Are you sure you want to delete this information?",
-//       [
-//         { text: "No", style: "cancel" },
-//         { text: "Yes", onPress: () => console.log("Information Deleted") },
-//       ],
-//       { cancelable: false }
-//     );
-//   };
-
-//   return (
-//     <KeyboardAvoidingView
-//       style={{ flex: 1 }}
-//       behavior={Platform.OS === "ios" ? "padding" : "height"}
-//       keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
-//     >
-//       <ScrollView contentContainerStyle={styles.container}>
-//         <View style={styles.container}>
-//           <Text style={styles.text}>Application Form</Text>
-//           <View style={styles.form}>
-//             <Text style={styles.label}>Name:</Text>
-//             <TextInput
-//               style={styles.input}
-//               placeholder="Enter your Name"
-//               value={name}
-//               onChangeText={setName}
-//             />
-//             {errors.name ? (
-//               <Text style={styles.errorText}>{errors.name}</Text>
-//             ) : null}
-//             <Text style={styles.label}>Arrive Date:</Text>
-//             <TextInput
-//               style={styles.input}
-//               placeholder="Enter your Arrival Date"
-//               value={arriveDate}
-//               onChangeText={setArriveDate}
-//             />
-//             {errors.arriveDate ? (
-//               <Text style={styles.errorText}>{errors.arriveDate}</Text>
-//             ) : null}
-//             <Text style={styles.label}>Hope to go district:</Text>
-//             <TextInput
-//               style={styles.input}
-//               placeholder="Enter your hope to go district"
-//               value={district}
-//               onChangeText={setDistrict}
-//             />
-//             {errors.district ? (
-//               <Text style={styles.errorText}>{errors.district}</Text>
-//             ) : null}
-//             <Text style={styles.label}>Contact No:</Text>
-//             <TextInput
-//               style={styles.input}
-//               placeholder="Enter your Contact No"
-//               value={contactNo}
-//               onChangeText={setContactNo}
-//             />
-//             {errors.contactNo ? (
-//               <Text style={styles.errorText}>{errors.contactNo}</Text>
-//             ) : null}
-//             <Text style={styles.label}>Passport Id:</Text>
-//             <TextInput
-//               style={styles.input}
-//               placeholder="Enter your Passport Id"
-//               value={passportId}
-//               onChangeText={setPassportId}
-//             />
-//             {errors.passportId ? (
-//               <Text style={styles.errorText}>{errors.passportId}</Text>
-//             ) : null}
-//             <Text style={styles.label}>Emergency No:</Text>
-//             <TextInput
-//               style={styles.input}
-//               placeholder="Enter your Emergency No"
-//               value={emergencyNo}
-//               onChangeText={setEmergencyNo}
-//             />
-//             {errors.emergencyNo ? (
-//               <Text style={styles.errorText}>{errors.emergencyNo}</Text>
-//             ) : null}
-//             <Text style={styles.label}>Comment:</Text>
-//             <TextInput
-//               style={styles.input}
-//               placeholder="Enter your Comment"
-//               value={comment}
-//               onChangeText={setComment}
-//             />
-//             {errors.comment ? (
-//               <Text style={styles.errorText}>{errors.comment}</Text>
-//             ) : null}
-//             <Text style={styles.label}>Do you like Tracking</Text>
-//             <View style={styles.buttonGroup}>
-//               <TouchableOpacity
-//                 style={[
-//                   styles.button,
-//                   likesTracking === "yes" ? styles.buttonSelected : null,
-//                 ]}
-//                 onPress={() => setLikesTracking("yes")}
-//               >
-//                 <Text style={styles.buttonText}>Yes</Text>
-//               </TouchableOpacity>
-//               <TouchableOpacity
-//                 style={[
-//                   styles.button,
-//                   likesTracking === "no" ? styles.buttonSelected : null,
-//                 ]}
-//                 onPress={() => setLikesTracking("no")}
-//               >
-//                 <Text style={styles.buttonText}>No</Text>
-//               </TouchableOpacity>
-//             </View>
-//             {errors.likesTracking ? (
-//               <Text style={styles.errorText}>{errors.likesTracking}</Text>
-//             ) : null}
-//             <View style={styles.actionButtonGroup}>
-//               <TouchableOpacity
-//                 style={styles.actionButton}
-//                 onPress={handleDone}
-//               >
-//                 <Text style={styles.actionButtonText}>Done</Text>
-//               </TouchableOpacity>
-//               <TouchableOpacity
-//                 style={styles.actionButton}
-//                 onPress={handleEdit}
-//               >
-//                 <Text style={styles.actionButtonText}>Edit</Text>
-//               </TouchableOpacity>
-//               <TouchableOpacity
-//                 style={styles.actionButton}
-//                 onPress={handleDelete}
-//               >
-//                 <Text style={styles.actionButtonText}>Delete</Text>
-//               </TouchableOpacity>
-//             </View>
-//           </View>
-//         </View>
-//       </ScrollView>
-//     </KeyboardAvoidingView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flexGrow: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   text: {
-//     fontSize: 24,
-//     fontWeight: "bold",
-//     paddingTop: 20,
-//     marginBottom: 16,
-//   },
-//   form: {
-//     backgroundColor: "white",
-//     padding: 20,
-//     width: screenWidth - 50,
-//     borderRadius: 10,
-//     shadowColor: "black",
-//     shadowOffset: {
-//       width: 0,
-//       height: 2,
-//     },
-//     shadowOpacity: 0.25,
-//     shadowRadius: 4,
-//     elevation: 5,
-//   },
-//   label: {
-//     fontSize: 16,
-//     marginBottom: 5,
-//     fontWeight: "bold",
-//   },
-//   input: {
-//     height: 40,
-//     borderColor: "#ddd",
-//     borderWidth: 1,
-//     marginBottom: 15,
-//     padding: 10,
-//     borderRadius: 5,
-//   },
-//   errorText: {
-//     color: "red",
-//     marginBottom: 10,
-//   },
-//   buttonGroup: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     marginBottom: 15,
-//   },
-//   button: {
-//     backgroundColor: "#ddd",
-//     padding: 10,
-//     width: 100,
-//     alignItems: "center",
-//     borderRadius: 5,
-//   },
-//   buttonText: {
-//     fontSize: 16,
-//     color: "#000",
-//   },
-//   buttonSelected: {
-//     backgroundColor: "#007BFF",
-//     color: "#fff",
-//   },
-//   actionButtonGroup: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     marginTop: 20,
-//   },
-//   actionButton: {
-//     backgroundColor: "grey",
-//     padding: 10,
-//     width: 80,
-//     alignItems: "center",
-//     borderRadius: 5,
-//   },
-//   actionButtonText: {
-//     fontSize: 16,
-//     color: "#fff",
-//   },
-// });
-
-
 import {
   Dimensions,
   StyleSheet,
@@ -302,6 +27,8 @@ export default function ApplicationForm() {
   const [comment, setComment] = useState("");
   const [likesTracking, setLikesTracking] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [isEditable, setIsEditable] = useState(true);
+  const [isFormFilled, setIsFormFilled] = useState(false);
   const [errors, setErrors] = useState({});
 
   const isFocused = useIsFocused();
@@ -309,17 +36,51 @@ export default function ApplicationForm() {
     useEffect(() => {
   
       const fetchUserId = async () => {
-        const id = await getUserId();
-        setUserId(id);
+        const userId = await getUserId();
+        if (userId){
+          setUserId(userId);
+          try{
+            const response = await fetch('https://piglet-vital-alien.ngrok-free.app/formData', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ userId })
+            });
+            
+            const data = await response.json();
+            
+            if (response.status === 200){
+              console.log("success", data);
+              setName(data.name);
+              setArriveDate(data.arriveDate);
+              setDistrict(data.district);
+              setContactNo(data.contactNo);
+              setPassportId(data.passportId);
+              setEmergencyNo(data.emergencyNo);
+              setComment(data.comment);
+              setLikesTracking(data.likesTracking);
+              setIsFormFilled(true);
+              setIsEditable(false);
+              console.log("done");
+            } else {
+              setIsFormFilled(false);
+              setIsEditable(true); 
+            }
+          } 
+          catch (error) {
+            console.error('Error!', error);
+          }
+        }
       };
   
       fetchUserId();
     }, [isFocused]);
 
   const handleDone = async () => {
-    // const userId = await getUserId();
     console.log(userId);
     const formData = {
+        userId,
         name,
         arriveDate,
         district,
@@ -327,14 +88,16 @@ export default function ApplicationForm() {
         passportId,
         emergencyNo,
         comment,
-        likesTracking,
-        userId
+        likesTracking
     };
+
+    const endpoint = isFormFilled ? 'https://piglet-vital-alien.ngrok-free.app/edit-form' : 'https://piglet-vital-alien.ngrok-free.app/submit-form';
+    const method = isFormFilled ? 'PUT' : 'POST';
 
     try {
         console.log(JSON.stringify(formData));
-        const response = await fetch('https://piglet-vital-alien.ngrok-free.app/submit-form', {
-            method: 'POST',
+        const response = await fetch(endpoint, {
+            method: method,
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -343,19 +106,22 @@ export default function ApplicationForm() {
 
         const responseJson = await response.json();
         if (response.ok) {
-            Alert.alert("Success", "Form submitted successfully!");
+            setIsEditable(false);
+            setIsFormFilled(true);
+            Alert.alert("Success", isFormFilled ? "Form updated successfully!" : "Form submitted successfully!");
         } else {
             throw new Error(responseJson.message);
         }
     } catch (error) {
         console.error("Submission failed", error); 
-        Alert.alert("Error", "Failed to submit form");
+        Alert.alert("Error", "Failed to " + (isFormFilled ? "update" : "submit") + " form");
     }
 };
 
   const handleEdit = async () => {
     const id = await getUserId();
     console.log(id);
+    setIsEditable(true);
     Alert.alert("Edit", "You can now edit the form.");
   };
 
@@ -365,11 +131,52 @@ export default function ApplicationForm() {
       "Are you sure you want to delete this information?",
       [
         { text: "No", style: "cancel" },
-        { text: "Yes", onPress: () => console.log("Information Deleted") },
+        { text: "Yes", onPress: () => {
+          deleteFormData();
+          console.log("Information Deleted");
+        }},
       ],
       { cancelable: false }
     );
   };
+
+  const deleteFormData = async () => {
+    const userId = await getUserId();
+    if (userId) {
+        try {
+            const response = await fetch('https://piglet-vital-alien.ngrok-free.app/delete-form', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ userId }),
+            });
+
+            const responseData = await response.json();
+            if (response.ok) {
+                setName('');
+                setArriveDate('');
+                setDistrict('');
+                setContactNo('');
+                setPassportId('');
+                setEmergencyNo('');
+                setComment('');
+                setLikesTracking(null);
+                setIsFormFilled(false);
+                setIsEditable(true); 
+                console.log('Form data deleted successfully');
+                Alert.alert("Success", "Form data deleted successfully");
+            } else {
+                throw new Error(responseData.message);
+            }
+        } catch (error) {
+            console.error("Error deleting form data:", error);
+            Alert.alert("Error", "Failed to delete form data");
+        }
+    } else {
+        Alert.alert("Error", "User ID is not available");
+    }
+};
 
   return (
     <KeyboardAvoidingView
@@ -387,6 +194,7 @@ export default function ApplicationForm() {
               placeholder="Enter your Name"
               value={name}
               onChangeText={setName}
+              editable={isEditable} 
             />
             {errors.name ? (
               <Text style={styles.errorText}>{errors.name}</Text>
@@ -397,6 +205,7 @@ export default function ApplicationForm() {
               placeholder="Enter your Arrival Date"
               value={arriveDate}
               onChangeText={setArriveDate}
+              editable={isEditable}
             />
             {errors.arriveDate ? (
               <Text style={styles.errorText}>{errors.arriveDate}</Text>
@@ -407,6 +216,7 @@ export default function ApplicationForm() {
               placeholder="Enter your hope to go district"
               value={district}
               onChangeText={setDistrict}
+              editable={isEditable}
             />
             {errors.district ? (
               <Text style={styles.errorText}>{errors.district}</Text>
@@ -417,6 +227,7 @@ export default function ApplicationForm() {
               placeholder="Enter your Contact No"
               value={contactNo}
               onChangeText={setContactNo}
+              editable={isEditable}
             />
             {errors.contactNo ? (
               <Text style={styles.errorText}>{errors.contactNo}</Text>
@@ -427,6 +238,7 @@ export default function ApplicationForm() {
               placeholder="Enter your Passport Id"
               value={passportId}
               onChangeText={setPassportId}
+              editable={isEditable}
             />
             {errors.passportId ? (
               <Text style={styles.errorText}>{errors.passportId}</Text>
@@ -437,6 +249,7 @@ export default function ApplicationForm() {
               placeholder="Enter your Emergency No"
               value={emergencyNo}
               onChangeText={setEmergencyNo}
+              editable={isEditable}
             />
             {errors.emergencyNo ? (
               <Text style={styles.errorText}>{errors.emergencyNo}</Text>
@@ -447,6 +260,7 @@ export default function ApplicationForm() {
               placeholder="Enter your Comment"
               value={comment}
               onChangeText={setComment}
+              editable={isEditable}
             />
             {errors.comment ? (
               <Text style={styles.errorText}>{errors.comment}</Text>
@@ -459,6 +273,7 @@ export default function ApplicationForm() {
                   likesTracking === "yes" ? styles.buttonSelected : null,
                 ]}
                 onPress={() => setLikesTracking("yes")}
+                editable={isEditable}
               >
                 <Text style={styles.buttonText}>Yes</Text>
               </TouchableOpacity>
@@ -468,6 +283,7 @@ export default function ApplicationForm() {
                   likesTracking === "no" ? styles.buttonSelected : null,
                 ]}
                 onPress={() => setLikesTracking("no")}
+                editable={isEditable}
               >
                 <Text style={styles.buttonText}>No</Text>
               </TouchableOpacity>
@@ -485,12 +301,14 @@ export default function ApplicationForm() {
               <TouchableOpacity
                 style={styles.actionButton}
                 onPress={handleEdit}
+                disabled={!isFormFilled}
               >
                 <Text style={styles.actionButtonText}>Edit</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={styles.actionButton}
                 onPress={handleDelete}
+                disabled={!isFormFilled}
               >
                 <Text style={styles.actionButtonText}>Delete</Text>
               </TouchableOpacity>
